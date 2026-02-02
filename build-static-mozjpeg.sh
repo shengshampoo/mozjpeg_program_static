bd591fe43e0e7ff5e7812e6c44b325cbb0729694
shengshampoo <shengzh@tutanota.com>
2026-02-02 09:38:03 +0000

    Update build-static-mozjpeg.sh

--- build-static-mozjpeg.sh
+++ build-static-mozjpeg.sh
@@ -18,6 +18,7 @@ ninja
 ninja install
 
 # mozjpeg
+cd $WORKSPACE
 git clone https://github.com/mozilla/mozjpeg.git
 cd mozjpeg
 mkdir build
@@ -27,6 +28,7 @@ ninja
 ninja install
 
 # guetzli-cuda-opencl
+cd $WORKSPACE
 git clone https://github.com/ianhuang-777/guetzli-cuda-opencl.git
 cd guetzli-cuda-opencl
 CFLAGS=-static LDFLAGS="-static --static -no-pie -s -lpng16 -lz" make
@@ -34,9 +36,19 @@ cd bin/Release
 tar vcJf guetzlimm.tar.xz guetzli libguetzli_static.a
 mv ./guetzlimm.tar.xz /work/artifact/
 
+# libwebp
+cd $WORKSPACE
+git clone https://chromium.googlesource.com/webm/libwebp
+cd libwebp
+mkdir build
+cd build
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr/local/libwebpmm -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS="-static --static -no-pie -s" ..
+ninja
+ninja install
+
 cd /usr/local
 tar vcJf ./zopflimm.tar.xz zopflimm
 tar vcJf ./mozjpegmm.tar.xz mozjpegmm
+tar vcJf ./libwebpmm.tar.xz libwebpmm
 
-mv ./zopflimm.tar.xz /work/artifact/
-mv ./mozjpegmm.tar.xz /work/artifact/
+mv ./[lmz]*mm.tar.xz /work/artifact/
